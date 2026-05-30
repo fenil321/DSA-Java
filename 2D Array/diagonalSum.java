6f4274c63774a6a5ddb1dc23170d6f4dcf1049ca
fenil321 <fparmar163@gmail.com>
2026-05-30 20:49:19 +0530

    initial commit

---
+++ 2D Array/diagonalSum.java
@@ -0,0 +1,17 @@
+public class diagonalSum {
+    public static void main(String[] args) {
+        int[][] mat = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println(diagonalSum(mat));
+    }
+
+    public static int diagonalSum(int[][] mat) {
+        int sum = 0;
+        for (int i = 0; i < mat.length; i++) {
+            sum += mat[i][i];
+            if (i != mat.length - 1 - i) {
+                sum += mat[i][mat.length - 1 - i];
+            }
+        }
+        return sum;
+    }
+}
