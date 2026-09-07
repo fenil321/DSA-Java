e165cc22daccf5bb2bbbf125d06c7a0a3a0dd5dc
fenil321 <fparmar163@gmail.com>
2026-09-07 16:58:41 +0530

    Modify File content

--- Stack/StackwithLL.java
+++ Stack/StackwithLL.java
@@ -49,6 +49,7 @@ public class StackwithLL {
     }
     public static void main(String[] args) {
         Stack s = new Stack();
+        
         s.push(1);
         s.push(2);
         s.push(3);
