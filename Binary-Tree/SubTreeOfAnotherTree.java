a21681bd6d29d4537320b659802ccd283662bb40
fenil321 <fparmar163@gmail.com>
2026-09-07 16:53:06 +0530

    Modify File content

---
+++ Binary-Tree/SubTreeOfAnotherTree.java
@@ -0,0 +1,58 @@
+public class SubTreeOfAnotherTree {
+    static class Node {
+        int data;
+        Node left, right;
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    public static boolean isIdentical(Node root, Node subRoot){
+        if(root==null && subRoot==null){
+            return true;
+        }
+        else if(root==null || subRoot==null || root.data!=subRoot.data){
+            return false;
+        }
+
+        if(!isIdentical(root.left, subRoot.left)){
+            return false;
+        }
+        if(!isIdentical(root.right, subRoot.right)){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isSubtree(Node root, Node subRoot) {
+        if(root==null){
+            return false;
+        }
+        if(root.data==subRoot.data){
+           if(isIdentical(root, subRoot)){
+               return true;
+           }
+        }
+    //    boolean leftResult = isSubtree(root.left, subRoot);
+    //    boolean rightResult = isSubtree(root.right, subRoot);
+       
+    //    return leftResult || rightResult;
+       return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        Node subRoot = new Node(2);
+        subRoot.left = new Node(4);
+        subRoot.right = new Node(5);
+
+        System.out.println(isSubtree(root, subRoot));
+    }
+}
