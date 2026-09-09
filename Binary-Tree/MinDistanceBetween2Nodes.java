88b74a68a656330f3d980aa8752299667200ad07
fenil321 <fparmar163@gmail.com>
2026-09-09 17:18:46 +0530

    Modify File content

---
+++ Binary-Tree/MinDistanceBetween2Nodes.java
@@ -0,0 +1,78 @@
+public class MinDistanceBetween2Nodes {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node lca(Node root, int n1, int n2){
+        if(root==null){
+            return null;
+        }
+
+        if(root.data==n1 || root.data==n2){
+            return root;
+        }
+
+        Node leftLca=lca(root.left, n1, n2);
+        Node rightLca=lca(root.right, n1, n2);
+
+       //leftLca=val and rightLca=null
+       if(rightLca==null){
+            return leftLca;
+        }
+        //leftLca=null and rightLca=val
+        if(leftLca==null){
+            return rightLca;
+        }
+        //leftLca=val and rightLca=val
+        return root;
+    }
+
+    public static int lcaDist(Node root, int n){
+        if(root==null){
+            return -1;
+        }
+        if(root.data==n){
+            return 0;
+        }
+
+        int leftDist=lcaDist(root.left, n);
+        int rightDist=lcaDist(root.right, n);
+
+        if(leftDist==-1 && rightDist==-1){
+            return -1;
+        }
+        else if(leftDist==-1){
+            return rightDist+1;
+        }
+        else{
+            return leftDist+1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2){
+        Node lca=lca(root, n1, n2);
+        int d1=lcaDist(lca, n1);
+        int d2=lcaDist(lca, n2);
+        return d1+d2;
+    }
+
+    public static void main(String args[]){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int n1 = 4, n2 = 5;
+        System.out.println("Dist(4, 5) = " + minDist(root, n1, n2));
+    }
+}
