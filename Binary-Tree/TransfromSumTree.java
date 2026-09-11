260e5a9f177b848689df2f02bf72daab73ccc0ba
fenil321 <fparmar163@gmail.com>
2026-09-11 16:56:23 +0530

    Modify File content

---
+++ Binary-Tree/TransfromSumTree.java
@@ -0,0 +1,47 @@
+public class TransfromSumTree {
+    public static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    public static int transfrom(Node root){
+        if(root == null){
+            return 0;
+        }   
+        int leftChild = transfrom(root.left);
+        int rightChild = transfrom(root.right); 
+        int oldValue = root.data;
+
+        int newLeft=root.left==null?0:root.left.data;
+        int newRight=root.right==null?0:root.right.data;
+
+        root.data = newLeft + leftChild + newRight + rightChild;
+        return oldValue;
+    }
+    public static void preorder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        transfrom(root);
+        preorder(root);
+        
+    }
+}
