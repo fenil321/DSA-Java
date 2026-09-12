f1854c7be74b49b076f2bce48de328c92ef0b885
fenil321 <fparmar163@gmail.com>
2026-09-12 18:43:10 +0530

    Modify File content

--- BST/PrintInRange.java
+++ BST/PrintInRange.java
@@ -1,4 +1,4 @@
-public class BuildBST {
+public class PrintInRange {
     static class Node {
         int data;
         Node left, right;
@@ -33,6 +33,9 @@ public class BuildBST {
         inoder(root.right);
     }
     public static void printInRange(Node root,int k1,int k2){
+        if(root==null){
+            return;
+        }
         if(root.data>=k1 && root.data<=k2){
             printInRange(root.left,k1,k2);
             System.out.print(root.data+" ");
@@ -45,7 +48,7 @@ public class BuildBST {
         }
     }
     public static void main(String[] args) {
-        int[] arr = {5,1,3,4,2,7};
+        int[] arr = {8,5,3,1,4,6,10,11,14};
         Node root = null;
         for(int i=0;i<arr.length;i++){
             root=insert(root,arr[i]);
@@ -53,5 +56,7 @@ public class BuildBST {
 
         inoder(root);
         System.out.println();
+
+        printInRange(root,5,12);
     }
 }
